25aeb46ddb917eebc017d93c60ac5d8a1f396cf1
Dan-Blanchette <blan5568@vandals.uidaho.edu>
2023-06-02 11:14:14 -0700

    added test function for sorting specimens in each frame batch.

--- frame_sort/fs.py
+++ frame_sort/fs.py
@@ -11,36 +11,44 @@ import shutil
 
 
 
+def specimen_sort():
+   specimen = 1
+   # file_sort()
+   for i in range(1, 115):
+      # print(i % 19)
+      if i % 19 == 0:
+         print("Specimen_" + str(specimen))
+         specimen += 1
 
 
-# def file_sort():
+def file_sort():
 
-#     # open green channel image folder
-#     path_to_files = '../green_chan_imgs/'
+    # open green channel image folder
+    path_to_files = '../green_chan_imgs/'
 
-#     # get the total number of files in the folder
-#     # shoould = num_specimens * num_frames * tot_zPos
-#     tot_files = len([name for name in os.listdir(path_to_files)
-#                 if os.path.isfile(os.path.join(path_to_files, name))])
-#     # print(tot_files)
+    # get the total number of files in the folder
+    # shoould = num_specimens * num_frames * tot_zPos
+    tot_files = len([name for name in os.listdir(path_to_files)
+                if os.path.isfile(os.path.join(path_to_files, name))])
+    # print(tot_files)
 
-#     frame_no = 1
-#     # a list of object codes which allow the program to sort data based on string matching
-#     object_codes = ['_T']
-#     for i in range(tot_files):
-#         # if the remainder of i mod (total images per frame per specimen) is 0
-#         '''NOTE: value 114 was calculated based on # number of specimens * total number of z-positions in each frame series'''
-#         if i % 114 == 0:
-#             # print("Frames_" + str(frame_no))
-#             # increment the frame count
-#             frame_no = (frame_no + 1)
-#             # take the file folder path and name the folders frames_ concatonated with the modulus count
-#             frameFolderPath = os.path.join(
-#                 path_to_files, 'frames_' + str(frame_no))
-#         # if a directory name frames_+frame_no does not exist already
-#         if not os.path.isdir(frameFolderPath):
-#             # make the directory and name it
-#             os.mkdir(frameFolderPath)
+    frame_no = 1
+    # a list of object codes which allow the program to sort data based on string matching
+    object_codes = ['_T']
+    for i in range(tot_files):
+        # if the remainder of i mod (total images per frame per specimen) is 0
+        '''NOTE: value 114 was calculated based on # number of specimens * total number of z-positions in each frame series'''
+        if i % 114 == 0:
+            # print("Frames_" + str(frame_no))
+            # increment the frame count
+            frame_no = (frame_no + 1)
+            # take the file folder path and name the folders frames_ concatonated with the modulus count
+            frameFolderPath = os.path.join(
+                path_to_files, 'frames_' + str(frame_no))
+        # if a directory name frames_+frame_no does not exist already
+        if not os.path.isdir(frameFolderPath):
+            # make the directory and name it
+            os.mkdir(frameFolderPath)
 
 
         
@@ -58,13 +66,10 @@ import shutil
 # read file all contents for each specimen and sort them into their respective frame folder
 # increment to the next frame identifier and continue until end of folder
 def main():
-    specimen = 1
-    # file_sort()
-    for i in range(1, 115):
-        # print(i % 19)
-        if i % 19 == 0:
-            print("Specimen_" + str(specimen))
-            specimen += 1
+   
+   # file_sort()
+   specimen_sort()
+
 
 
 
