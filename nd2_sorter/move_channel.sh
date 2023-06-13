05a748066eb4ff631df23b70e02d43906b94b628
Dan-Blanchette <blan5568@vandals.uidaho.edu>
2023-06-13 16:09:09 -0700

    Pipeline implementation complete and working.

--- nd2_sorter/move_channel.sh
+++ nd2_sorter/move_channel.sh
@@ -63,3 +63,20 @@ for i in {1..6}; do
       echo "Please adjust the maximum range to reflect the total number of specimens."
    fi
 done
+
+# FRAME SORT
+
+cd "$destination"
+
+for i in {1..6}; do
+   cd "spec_$i/"
+   for j in {1..97}; do
+      mkdir -p "frame_$j/"
+      if [ $j -lt 10 ]; then
+         mv *T0"$j"* "frame_$j/"
+      else
+         mv *T"$j"* "frame_$j/"
+      fi
+   done
+   cd ../
+done
