257d17d8ded2a554aac739281394cd33c4f7a061
Dan-Blanchette <blan5568@vandals.uidaho.edu>
2023-06-13 09:38:05 -0700

    this script is tested and working

---
+++ nd2_sorter/move_channel.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Read user input for the pattern
+echo "Please enter the pattern for files to move:"
+read -r pattern
+
+echo "What file extension would you like to move?:"
+echo "Ex: .png, .jpeg, .tiff, .tif"
+read -r extension
+
+echo "Please enter the destination name for files to move:"
+read -r destination
+mkdir -p "$destination"
+
+# Enable nullglob to handle cases where no files match the pattern
+shopt -s nullglob
+
+cwd=$(pwd)
+# Store the matching filenames in an array
+# wild_cards are placed before and after the pattern
+files=("$cwd"/tif_3chan/*"$pattern"*"$extension")
+
+# Check if any files match the pattern
+if [ ${#files[@]} -eq 0 ]; then
+    echo "No files found matching the pattern: $pattern"
+else
+    # Move each file to the destination directory
+    for file in "${files[@]}"; do
+        mv "$file" "$destination"
+    done
+
+    echo "Files moved successfully."
+fi
+
+# Disable nullglob
+shopt -u nullglob
+
+cd "$dir_name" || exit
+touch frame_sort.sh
